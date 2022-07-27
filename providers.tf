4ad986b13018365cce2a2eed0908737ff2958206
“benesnpl” <bene@otenet.gr>
2022-07-27 13:15:20 +0300

    Finals

--- providers.tf
+++ providers.tf
@@ -10,8 +10,8 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = ""
-  client_id = ""
-  client_secret = ""
-  tenant_id = ""
+  subscription_id = "643419c7-11b4-43c2-9d6a-ee115f86627f"
+  client_id = "ebd473d6-bd85-4025-9efa-1069c13f3939"
+  client_secret = "Hjn8Q~0mU1bI2arAZOruXl1id4oLxFytD0Z82cBI"
+  tenant_id = "cf668d98-df58-45c9-86fb-b72c2761364b"
 }
